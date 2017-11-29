9c05382d912acff19ef38847e04661cd78a4ae4e
abdulazizabubakar <33334002+abdulazizabubakar@users.noreply.github.com>
2017-11-29 01:42:12 +0100

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,17 +1,15 @@
-         
-## Create Plot 4
-  par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-  with(t, {
-    plot(Global_active_power~dateTime, type="l", 
-         ylab="Global Active Power (kilowatts)", xlab="")
-    plot(Voltage~dateTime, type="l", 
-         ylab="Voltage (volt)", xlab="")
-    plot(Sub_metering_1~dateTime, type="l", 
-         ylab="Global Active Power (kilowatts)", xlab="")
-    lines(Sub_metering_2~dateTime,col='Red')
-    lines(Sub_metering_3~dateTime,col='Blue')
-    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-    plot(Global_reactive_power~dateTime, type="l", 
-         ylab="Global Rective Power (kilowatts)",xlab="")
-  })
+# Plot 4
+# open png device
+
+png(filename = './figures/plot4.png', width = 480, height = 480, units='px')
+
+# plot figure
+par(mfrow = c(2, 2))
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+plot(data$DateTime, data$Voltage, xlab = 'datetime', ylab = 'Voltage', type = 'l')
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+plot(data$DateTime, data$Global_reactive_power, xlab = 'datetime', ylab = 'Global_reactive_power', type = 'l')
+dev.off()
