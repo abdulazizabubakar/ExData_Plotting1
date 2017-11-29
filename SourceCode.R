0e9108cd5b0c1fd81925e856fe1a730500dfcf00
abdulazizabubakar <33334002+abdulazizabubakar@users.noreply.github.com>
2017-11-29 01:37:42 +0100

    Update SourceCode.R

--- SourceCode.R
+++ SourceCode.R
@@ -1,24 +1,12 @@
 
-t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+# download and unzip data
+setwd('E:\\Dropbox\\coursera\\Exploratory Data Analysis')
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+unzip('./data/household_power_consumption.zip', exdir = './data')
 
-## Format date 
-t$Date <- as.Date(t$Date, "%d/%m/%Y")
-  
-## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
-t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
-  
-## Remove incomplete observation
-t <- t[complete.cases(t),]
+# read data into R
+files <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
 
-dateTime <- paste(t$Date, t$Time)
-  
-dateTime <- setNames(dateTime, "DateTime")
-  
-## Remove Date and Time column
-t <- t[ ,!(names(t) %in% c("Date","Time"))]
-  
-## Add DateTime column
-t <- cbind(dateTime, t)
-  
-## Format dateTime Column
-t$dateTime <- as.POSIXct(dateTime)
