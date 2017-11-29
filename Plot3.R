f60644e1517e9f5a7e518e5910dbbed3cb9a4862
abdulazizabubakar <33334002+abdulazizabubakar@users.noreply.github.com>
2017-11-29 01:40:02 +0100

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,10 +1,15 @@
-## Create Plot 3
-  with(t, {
-    plot(Sub_metering_1~dateTime, type="l",
-         ylab="Global Active Power (kilowatts)", xlab="")
-    lines(Sub_metering_2~dateTime,col='Red')
-    lines(Sub_metering_3~dateTime,col='Blue')
-  })
-  legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
-         c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-         
+
+
+# Plot 3
+# open device
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot3.png', width = 480, height = 480, units='px')
+# plot figure
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+# close device
+dev.off()
+
